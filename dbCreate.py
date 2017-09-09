2fc9a2c00752a8d22dc44024d520f3def9340bc8
MF <notsure@compuserv.com>
2017-09-09 18:29:18 -0500

    set up menu display and create file

---
+++ dbCreate.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+def createTable():
+    filename = "storeDB.sqlite"
+    table1 = "products"
+    idField = "prodID"
+    field1 = "Name"
+    field2 = "Price"
+    field3 = "Quantity"
+    field4 = "NumSold"
+    field5 = "Description"
+    field6 = "Color"
+
+    conn = sqlite3.connect(filename)
+    connCursor = conn.cursor()
+
+    connCursor.execute('CREATE TABLE {tn} ({f0} INTEGER PRIMARY KEY, {f1} TEXT, {f2} REAL, {f3} INTEGER, {f4} INTEGER, {f5} TEXT, {f6} TEXT)'\
+                       .format(tn=table1, f0=idField, f1=field1, f2=field2, f3=field3, f4=field4, f5=field5, f6=field6))
+
+    conn.commit()
+    conn.close()
--- menu.py
+++ menu.py
@@ -1,5 +1,19 @@
-
+import dbCreate, dbAdd, dbUpdate, dbDelete, dbShowAll, dbShowOne
 
 def main():
-    print()
+    keep_running = True
+    while keep_running:
+        showMenu()
+
+def showMenu():
+    print("=" * 45)
+    print("Enter the number next to the menu item you want:")
+    print("1 -- Create Table")
+    print("2 -- Add New Entry")
+    print("3 -- Update Entry")
+    print("4 -- Delete Entry")
+    print("5 -- Show Table")
+    print("6 -- Show Entry")
+    print("Press any other key to exit.")
+    print("=" * 45)
 main()
