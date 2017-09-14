01512ed8b9493258dcb888b4b4409c9b0e694a71
not sure <notsure@compuserv.com>
2017-09-14 18:46:06 -0500

    added comments

--- dbUpdate.py
+++ dbUpdate.py
@@ -1,12 +1,13 @@
 import sqlite3
 
-
+# Defines main.
 def main(go = False):
     if go == True:
         updateEntry()
 
 
 def updateEntry():
+    # Variables are defined.
     filename = "storeDB.sqlite"
     table1 = "products"
     idField = "prodID"
@@ -17,12 +18,18 @@ def updateEntry():
     field5 = "Description"
     field6 = "Color"
 
+    # Connects to database.
     conn = sqlite3.connect(filename)
     connCursor = conn.cursor()
+
+    # Gets input from user.
     prodid = int(input("Enter product id of product to update:"))
+    # Runs function to get additional inputs from user.
     newData = getInputs()
 
+    # Exception handler.
     try:
+        # Combines statement string.
         connCursor.execute("UPDATE {tn} SET {c1}={v1}, {c2}={v2}, {c3}={v3}, {c4}={v4}, {c5}={v5},\
             {c6}={v6} WHERE {id}={v7}".format(tn=table1, c1=field1, v1=newData[0], c2=field2, \
             v2=float(newData[1]), c3=field3, v3=int(newData[2]), c4=field4, v4=int(newData[3]), \
@@ -30,10 +37,12 @@ def updateEntry():
     except sqlite3.Error:
         print("Error")
 
+    # Executes statement and closes connection.
     conn.commit()
     conn.close()
 
 
+# Function to get inputs from user.
 def getInputs():
     name = input("Enter new name:")
     price = input("Enter new price:")
